ecc760698295d2c858addc622179e6af7cf72a68
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-07-09 21:09:08 -0300

    Update script.js
    Adicionado os tipos de dados e suas propriedades

--- JavaScript Manual/script.js
+++ JavaScript Manual/script.js
@@ -18,3 +18,24 @@ const pattern = "ECMAScript"; // Esse tipo de variável sempre deve ser iniciali
 console.log(Myname);
 console.log(language);
 console.log(pattern);
+
+//Objetos, Arrays e Function
+
+//Para declarar um objeto faz se o seguinte comando
+var Person = {
+    //aqui você coloca os atributos do seu objeto
+    //chave, valor
+    "nome": "Matt",
+    "Idade": 23
+}
+
+//Para declarar Arrays, usa-se o seguinte comando
+var students = ["Matt", "Stefane", "Andreina", "Jeimison"];
+
+//Para declarar functions, usa-se o seguinte comando
+function Sum(a, b){
+    return a + b;
+}
+
+//O operador Typeof retorna o tipo da variável
+console.log(typeof Myname);
